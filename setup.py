84520025b90a702b13a73c332effc47de3e02152
Louise Lalu <louiserafaellalu@gmail.com>
2023-11-23 23:22:02 +0800

    add more configurations in pyproject.toml

--- setup.py
+++ setup.py
@@ -6,17 +6,14 @@ LONG_DESCRIPTION = 'A custom preprocessing package made for preparing data to be
 
 # Setting up
 setup(
-       # the name must match the folder name 'verysimplemodule'
-        name="jobfindr_preprocessing", 
+        name="preprocessing", 
         version=VERSION,
         author="Louise Lalu",
         author_email="louiserafaellalu@gmail.com",
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         packages=find_packages(),
-        install_requires=[], # add any additional packages that 
-        # needs to be installed along with your package. Eg: 'caer'
-        
+        install_requires=["datetime"],
         keywords=['preprocessing', 'custom'],
         classifiers= [
             "Development Status :: 3 - Alpha",
