26ef50b8ea753bf6d0267e21b4904f5682ffeeda
Louise Lalu <louiserafaellalu@gmail.com>
2023-11-24 00:19:06 +0800

    add py_modules option in setup.py

--- setup.py
+++ setup.py
@@ -10,10 +10,15 @@ setup(
         version=VERSION,
         author="Louise Lalu",
         author_email="louiserafaellalu@gmail.com",
+        url="https://github.com/conraduouou/job-findr-preprocessing",
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         packages=find_packages(),
         install_requires=[],
+        py_modules=[
+            "degree_labels",
+            "preprocessing"
+        ],
         keywords=['preprocessing', 'custom'],
         classifiers= [
             "Development Status :: 3 - Alpha",
